d434b8dd4a7fdcc0483d3b70dbb93ba6ca4ed967
Paula <pauzam003@gmail.com>
2026-09-15 10:51:57 +0200

    Se crea la pagina 'CarSelectionPage'

---
+++ pages/CarSelectionPage.ts
@@ -0,0 +1,33 @@
+import { Page, Locator } from '@playwright/test'
+
+export class CarSelectionPage{
+    readonly page: Page;
+    readonly carouselItems: Locator;
+    readonly bookButton: Locator;
+
+constructor(page: Page){
+    this.page = page;
+    this.carouselItems = page.locator('.ct-cars-carousel-car');
+    this.bookButton = page.locator('[data-auto-id="bookButton"]');
+
+}
+
+async selectFirstSuv(){
+    await this.carouselItems
+    .filter({ hasText: 'SUV' })
+    .first()
+    .click();
+
+}
+
+async confirmCarSelection(): Promise<Page>{
+
+    const popupPromise = this.page.waitForEvent('popup');
+    await this.bookButton
+    .first()
+    .click();
+    const newPage = await popupPromise;
+
+   return newPage;
+}
+} 
