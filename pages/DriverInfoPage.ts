2b0541bd433ad7c18044d7c20c5845bfe36d2524
Paula <pauzam003@gmail.com>
2026-09-15 12:03:15 +0200

    Se hace modificacion del archivo DriverInfoPage y se elimina el ejemplo de test: example.spec.ts

---
+++ pages/DriverInfoPage.ts
@@ -0,0 +1,18 @@
+import { Page, Locator } from '@playwright/test';
+
+export class DriverInfoPage {
+  readonly page: Page;
+  readonly coverageText: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.coverageText = page.locator('[data-auto-id="txtInsuranceDetails"]');
+  }
+
+  async getCoverageText(): Promise<string> {
+    const text = await this.coverageText.innerText();
+
+    return text;
+    
+  }
+}
